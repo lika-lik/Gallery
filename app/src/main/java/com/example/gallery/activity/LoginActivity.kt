c3d64496f342c839c16c4ad2ab5859380a5288ef
Малинина Анжелика <an.mal20115@gmail.com>
2022-07-08 17:49:46 +0300

    add login layout

--- app/src/main/java/com/example/gallery/activity/LoginActivity.kt
+++ app/src/main/java/com/example/gallery/activity/LoginActivity.kt
@@ -1,11 +1,59 @@
 package com.example.gallery.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.view.View
+import android.view.View.OnFocusChangeListener
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.TextFieldDefaults
+import com.example.gallery.R
+import com.example.gallery.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonEnter.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                validation()
+            }
+
+        })
     }
+
+    fun validation(): Boolean{
+        val pattern = Regex("[+]?[78]?[() 0-9-]+")
+        val login: String = binding.inputLogin.text.toString()
+        val password: String = binding.inputPassword.text.toString()
+        var validation: Boolean = true
+        if (login.isEmpty()) {
+            binding.textFieldLogin.error = getString(R.string.empty_field_warning)
+            validation = false
+        }else if(!pattern.matches(login)){
+            binding.textFieldLogin.error = getString(R.string.phone_format_warning)
+            validation = false
+        }
+
+        if(password.isEmpty()){
+            binding.textFieldPassword.error = getString(R.string.empty_field_warning)
+            validation = false
+        }else if (password.length < 6 || password.length > 255){
+            binding.textFieldPassword.error = getString(R.string.password_length_warning)
+            validation = false
+        }
+
+        if (validation) {
+            binding.textFieldLogin.error = null
+            binding.textFieldPassword.error = null
+        }
+        return validation
+    }
+
 }
--- app/src/main/java/com/example/gallery/activity/MainActivity.kt
+++ app/src/main/java/com/example/gallery/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gallery.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,19 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val call: Call<LoginResponse> = RetrofitClient.retrofitServices.getUser("+71234567890", "qwerty")
-        call.enqueue(object : Callback<LoginResponse> {
-            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                Log.d("MY123", "onFailure")
-            }
 
-            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
 
-                Log.d("MY123", "onResponse")
-                Log.d("MY123", response.body().toString())
-            }
-        })
-        Log.d("MY123", call.toString())
+        val randomIntent = Intent(this, LoginActivity::class.java)
+        startActivity(randomIntent)
+
+//        val call: Call<LoginResponse> = RetrofitClient.retrofitServices.getUser("+71234567890", "qwerty")
+//        call.enqueue(object : Callback<LoginResponse> {
+//            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+//                Log.d("MY123", "onFailure")
+//            }
+//
+//            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+//
+//                Log.d("MY123", "onResponse")
+//                Log.d("MY123", response.body().toString())
+//            }
+//        })
+//        Log.d("MY123", call.toString())
 
 
     }
